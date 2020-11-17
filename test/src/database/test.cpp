618d6ee88ae4877bde8c40361632444324107888
Bertalan Körmendy <korber314@gmail.com>
2020-11-17 18:35:41 +0100

    add gtest

---
+++ test/src/database/test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Berci on 2020. 11. 17..
+//
+
+#include "gtest/gtest.h"
+
+TEST(MyTestCategory, Vec2DAdditionTest){
+    EXPECT_EQ(1, 1);
+}
+
+TEST(MyTestCategory, Vec3DAdditionTest){
+    EXPECT_EQ(3, 3);
+}
---
+++ test/src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Berci on 2020. 11. 17..
+//
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
