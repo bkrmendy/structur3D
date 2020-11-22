8b02ac87a34e5255f93735d369768282f87c9dcf
Bertalan Körmendy <korber314@gmail.com>
2020-11-22 19:55:27 +0100

    add attribute

---
+++ include/data/Attribute.h
@@ -0,0 +1,17 @@
+//
+// Created by Berci on 2020. 11. 22..
+//
+
+#ifndef STRUCTUR3D_BASE_ATTRIBUTE_H
+#define STRUCTUR3D_BASE_ATTRIBUTE_H
+
+#include <variant>
+#include "Coord.h"
+#include "Base.h"
+#include "SetOperationType.h"
+
+namespace S3D {
+    using Attribute = std::variant<Coord, RADIUS, SetOperationType, std::string>;
+}
+
+#endif //STRUCTUR3D_BASE_ATTRIBUTE_H
