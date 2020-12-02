1635dc06d2547d1fffb00ec2893945e64e8396fb
Bertalan Körmendy <korber314@gmail.com>
2020-12-02 19:10:58 +0100

    add test skeleton

---
+++ test/src/serialization/unit_serialization.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Berci on 2020. 12. 02..
+//
+
+#include "gtest/gtest.h"
+
+TEST(SerializationTest, IDRoundTrip) {
+    FAIL() << "Not implemented";
+}
+
+TEST(SerializationTest, CoordRoundTrip) {
+    FAIL() << "Not implemented";
+}
+
+TEST(SerializationTest, SetOperationTypeRoundTrip) {
+    FAIL() << "Not implemented";
+}
+
+TEST(SerializationTest, NameRoundTrip) {
+    FAIL() << "Not implemented";
+}
