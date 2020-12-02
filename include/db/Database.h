c0d7b7f69075e6eb6beccdcab9ee167024834381
Bertalan Körmendy <korber314@gmail.com>
2020-12-02 18:26:02 +0100

    fix indent

--- include/db/Database.h
+++ include/db/Database.h
@@ -25,38 +25,37 @@
 #include "../data/SetOperationType.h"
 
 namespace S3D {
+    struct Database {
+        // Manipulation
+        virtual void upsert(const ID&, const Radius&, Timestamp timestamp) = 0;
+        virtual void retract(const ID&, const Radius&, Timestamp timestamp) = 0;
 
-struct Database {
-    // Manipulation
-    virtual void upsert(const ID&, const Radius&, Timestamp timestamp) = 0;
-    virtual void retract(const ID&, const Radius&, Timestamp timestamp) = 0;
+        virtual void upsert(const ID&, const Coord&, Timestamp timestamp) = 0;
+        virtual void retract(const ID&, const Coord&, Timestamp timestamp) = 0;
 
-    virtual void upsert(const ID&, const Coord&, Timestamp timestamp) = 0;
-    virtual void retract(const ID&, const Coord&, Timestamp timestamp) = 0;
+        virtual void upsert(const ID&, const SetOperationType&, Timestamp timestamp) = 0;
+        virtual void retract(const ID&, const SetOperationType&, Timestamp timestamp) = 0;
 
-    virtual void upsert(const ID&, const SetOperationType&, Timestamp timestamp) = 0;
-    virtual void retract(const ID&, const SetOperationType&, Timestamp timestamp) = 0;
+        virtual void upsert(const ID&, const Name& name, Timestamp timestamp) = 0;
+        virtual void retract(const ID&, const Name& name, Timestamp timestamp) = 0;
 
-    virtual void upsert(const ID&, const Name& name, Timestamp timestamp) = 0;
-    virtual void retract(const ID&, const Name& name, Timestamp timestamp) = 0;
+        virtual void connect(const ID&, const ID&, Timestamp timestamp) = 0;
+        virtual void disconnect(const ID&, const ID&, Timestamp timestamp) = 0;
+        virtual std::vector<ID> edges(const ID&) = 0;
 
-    virtual void connect(const ID&, const ID&, Timestamp timestamp) = 0;
-    virtual void disconnect(const ID&, const ID&, Timestamp timestamp) = 0;
+        virtual void create(const ID& entity, const NodeType& type, const ID& document, Timestamp timestamp) = 0;
+        virtual void remove(const ID& entity, const ID& document, Timestamp timestamp) = 0;
 
-    virtual void create(const ID& entity, const NodeType& type, const ID& document, Timestamp timestamp) = 0;
-    virtual void remove(const ID& entity, const ID& document, Timestamp timestamp) = 0;
+        // Querying
+        virtual std::vector<DocumentWithName> documents() = 0;
+        virtual std::vector<IDWithType> entities(const ID&) = 0;
 
-    // Querying
-    virtual std::vector<DocumentWithName> documents() = 0;
-    virtual std::vector<IDWithType> entities(const ID&) = 0;
+        virtual std::optional<Sphere> sphere(const ID& from) = 0;
+        virtual std::optional<SetOp> setop(const ID& from) = 0;
 
-    virtual std::optional<Sphere> sphere(const ID& from) = 0;
-    virtual std::optional<SetOp> setop(const ID& from) = 0;
 
-    virtual std::vector<ID> edges(const ID&) = 0;
-
-    virtual ~Database(){};
-};
+        virtual ~Database(){};
+    };
 }
 
 #endif /* database_h */
