78b14b5246cdebfeebe6a4862af7573891919653
Bertalan Körmendy <korber314@gmail.com>
2020-12-04 12:06:36 +0100

    update ddl with checks

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS radius (
-    deleted INTEGER NOT NULL DEFAULT 0,
+    deleted INTEGER NOT NULL DEFAULT 0 CHECK (deleted = 0 OR deleted = 1),
     timestamp UNSIGNED BIG INT NOT NULL,
     entity TEXT NOT NULL,
     magnitude REAL NOT NULL CHECK (magnitude >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS coord (
-    deleted INTEGER NOT NULL DEFAULT 0,
+    deleted INTEGER NOT NULL DEFAULT 0 CHECK (deleted = 0 OR deleted = 1),
     timestamp UNSIGNED BIG INT NOT NULL,
     entity TEXT NOT NULL,
     x real NOT NULL,
@@ -15,14 +15,14 @@ CREATE TABLE IF NOT EXISTS coord (
 );
 
 CREATE TABLE IF NOT EXISTS setoperationtype (
-    deleted INTEGER NOT NULL DEFAULT 0,
+    deleted INTEGER NOT NULL DEFAULT 0 CHECK (deleted = 0 OR deleted = 1),
     timestamp UNSIGNED BIG INT NOT NULL,
     entity TEXT NOT NULL,
     type INTEGER NOT NULL CHECK (type = 0 or type = 1 or type = 2)
 );
 
 CREATE TABLE IF NOT EXISTS edge (
-    deleted INTEGER NOT NULL DEFAULT 0,
+    deleted INTEGER NOT NULL DEFAULT 0 CHECK (deleted = 0 OR deleted = 1),
     timestamp UNSIGNED BIG INT NOT NULL,
     entity TEXT NOT NULL,
     entity_to TEXT NOT NULL
@@ -37,14 +37,14 @@ CREATE TABLE IF NOT EXISTS document (
 );
 
 CREATE TABLE IF NOT EXISTS name (
-    deleted INTEGER NOT NULL DEFAULT 0,
+    deleted INTEGER NOT NULL DEFAULT 0 CHECK (deleted = 0 OR deleted = 1),
     timestamp UNSIGNED BIG INT NOT NULL,
     entity TEXT NOT NULL,
     name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS attribute (
-    deleted INTEGER NOT NULL DEFAULT 0,
+    deleted INTEGER NOT NULL DEFAULT 0 CHECK (deleted = 0 OR deleted = 1),
     timestamp UNSIGNED BIG INT NOT NULL,
     entity TEXT NOT NULL,
     attrib BLOB NOT NULL,
